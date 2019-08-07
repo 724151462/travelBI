631078fd1c375885fc45637076223a5c39e1ec62
吴佳雄 <724151462@qq.com>
2019-08-07 18:09:06 +0800

    修改部分bug

--- myapp/public/js/map.js
+++ myapp/public/js/map.js
@@ -1,6 +1,6 @@
 var map = new AMap.Map('map-container', {
   resizeEnable: true, //是否监控地图容器尺寸变化
-  zoom:11, //初始化地图层级
+  zoom:13, //初始化地图层级
   center: [119.606184,26.101766] //初始化地图中心点
 });
 toolBar = new AMap.ToolBar({
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -256,4 +256,58 @@ router.get('/api/touristgender', function (req, res, next) {
     })
 });
 
+router.get('/api/wordcloud', function (req, res, next) {
+  res.json(
+    {
+      data: [
+        {
+          name: 'Sam S Club',
+          value: 10000,
+        }, {
+          name: 'Macys',
+          value: 6181
+        }, {
+          name: 'Amy Schumer',
+          value: 4386
+        }, {
+          name: 'Jurassic World',
+          value: 4055
+        }, {
+          name: 'Charter Communications',
+          value: 2467
+        }, {
+          name: 'Chick Fil A',
+          value: 2244
+        }, {
+          name: 'Planet Fitness',
+          value: 1898
+        }, {
+          name: 'Pitch Perfect',
+          value: 1484
+        }, {
+          name: 'Express',
+          value: 1112
+        }, {
+          name: 'Home',
+          value: 965
+        }, {
+          name: 'Johnny Depp',
+          value: 847
+        }, {
+          name: 'Lena Dunham',
+          value: 582
+        }, {
+          name: 'Lewis Hamilton',
+          value: 555
+        }, {
+          name: 'KXAN',
+          value: 550
+        }, {
+          name: 'Point Break',
+          value: 265
+        }]
+    })
+});
+
+
 module.exports = router;    
